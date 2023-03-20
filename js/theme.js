1ba6ba64b2aca750a54250aca5f0d751d08163d3
Amespay <95721679+Amespay@users.noreply.github.com>
2023-03-20 20:43:19 -0300

    Add files via upload

---
+++ js/theme.js
@@ -0,0 +1,44 @@
+(function($) {
+    'use strict';
+
+
+    if ($('.brand-carousel-one').length) {
+        $('.brand-carousel-one').owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: false,
+            navText: [
+                '<i class="fa fa-long-arrow-left"></i>',
+                '<i class="fa fa-long-arrow-right"></i>'
+            ],
+            dots: true,
+            autoWidth: false,
+            autoplay: true,
+            smartSpeed: 700,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                480: {
+                    items: 3
+                },
+                600: {
+                    items: 3
+                },
+                991: {
+                    items: 4
+                },
+                1000: {
+                    items: 5
+                },
+                1200: {
+                    items: 5
+                }
+            }
+        });
+    }
+
+
+})(jQuery);
